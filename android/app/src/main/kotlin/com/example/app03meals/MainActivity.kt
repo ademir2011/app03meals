46888a7a3ac9c0b3d4eec096bf14cab7935f403d
Ademir Bezerra <ademirbezerra2012@gmail.com>
2022-01-13 12:08:00 -0300

    Estrutura inicial com pastas estruturadas

---
+++ android/app/src/main/kotlin/com/example/app03meals/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app03meals
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,25 @@
+import 'package:app03meals/components/category_item.dart';
+import 'package:app03meals/data/dummy_data.dart';
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vamos cozinhar!'),
+      ),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES.map((cat) => CategoryItem(cat)).toList(),
+      ),
+    );
+  }
+}
